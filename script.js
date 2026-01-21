5465fae7a36feeca4725261821e4a505100c7ade
eileenwu-au <ewu150@gmail.com>
2026-01-21 16:32:04 +1100

    Practising if/else && NOT

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 // Practice if/else and &&
-let age = 93;
+let age = 91;
 
-if (age >= 14 && age <= 90) 
-    console.log("You are within the required age range of 14-90 years old");
-else
+if (!(age >= 14 && age <= 90)) 
     console.log("You are outside the required age range");
+else
+    console.log("You are within the required age range of 14-90 years old");
