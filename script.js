4ff10ae292f2c22dc57a42ec556acd2a2170d2a1
eileenwu-au <ewu150@gmail.com>
2026-01-21 14:42:33 +1100

    Practising with operators

--- script.js
+++ script.js
@@ -1,11 +1,14 @@
-let a = 10;
-
-console.log(a)
-
-a = 5;
-
-console.log(a)
-
-let b = 7 * a;
-
-console.log(b)
+let a = "" + 1 + 0; // 10
+let b = "" - 1 + 0; // -1
+let c = true + false; // 1
+let d = 6 / "3"; // 2
+let e = "2" * "3"; // 6
+let f = 4 + 5 + "px"; // 9px
+let g = "$" + 4 + 5; // $45
+let h = "4" - 2; // 2
+let i = "4px" - 2; // NaN
+let j = "  -9  " + 5; //   -9  5
+let k = "  -9  " - 5; // -14
+let l = null + 1; // 1
+let m = undefined + 1; // NaN
+let n = " \t \n" - 2; // 2
