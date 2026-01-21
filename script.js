9ecef2e42a8bef338464045a1d0a9d64ed878cfc
eileenwu-au <ewu150@gmail.com>
2026-01-21 16:15:16 +1100

    Practising if/else and &&

--- script.js
+++ script.js
@@ -1,28 +1,7 @@
-// Problem
-let a = "1"; // prompt("First number?", 1);
-let b = "2"; // prompt("Second number?", 2);
+// Practice if/else and &&
+let age = 93;
 
-alert(a + b); // 12
-
-// My solution
-//Ensure the numbers are recognised as numbers and not as strings
-
-let a = Number("1");
-let b = Number("2");
-let c = prompt("First number?", a);
-let d = prompt("Second number?", b);
-alert(a + b); // 3
-
-// Alternative solution 1
-// Add a unary plus to convert the string into a number on the prompt
-let a = +prompt("First number?", 1);
-let b = +prompt("Second number?", 2);
-
-alert(a + b); // 3
-
-// Alternative solution 2
-// Add a unary plus to convert the string into a number in the equation
-let a = prompt("First number?", 1);
-let b = prompt("Second number?", 2);
-
-alert(+a + +b); // 3
+if (age >= 14 && age <= 90) 
+    console.log("You are within the required age range of 14-90 years old");
+else
+    console.log("You are outside the required age range");
