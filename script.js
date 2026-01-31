cf6f7b7d087dba50f36f4368775dd12029e8ae55
eileenwu-au <ewu150@gmail.com>
2026-01-31 13:57:52 +1100

    Login check exercise

--- script.js
+++ script.js
@@ -1,7 +1,36 @@
-// Practice if/else and &&
-let age = 91;
+// Check the login exercise from https://javascript.info/logical-operators
 
-if (!(age >= 14 && age <= 90)) 
-    console.log("You are outside the required age range");
-else
-    console.log("You are within the required age range of 14-90 years old");
+// Step 1: prompt user for username
+let username = prompt("Enter your username");
+let password;
+
+// Step 2: check username input against conditions
+if (username == "Admin") {
+    let password = prompt("Enter your password");
+    // Step 3: check password input against conditions
+    if (password == "TheMaster") {
+        alert("Welcome!");
+    }
+
+    else if (password == null || password == "") {
+        console.log(password); // ! Displays the characters entered the first time
+        alert("Cancelled"); 
+    }
+
+    else {
+        alert("Wrong password");
+        console.log(password)
+    }
+}
+
+else if (username == undefined || username == "") {
+    alert("Cancelled");
+}
+
+else {
+    alert("I don't know you");
+}
+
+
+// && returns the first falsy value 
+// || returns the first truthy value
